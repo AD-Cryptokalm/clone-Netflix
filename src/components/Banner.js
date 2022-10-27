3ed27fcad16364fc153d47d990cba4d962b814fe
AD-code <cryptokalm82@gmail.com>
2022-10-27 15:45:52 +0200

    backgroung img banner ok

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Banner.scss";
+// import requestsApi from "../config/RequestsApi";
+import axios from "axios";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+  //   console.log(requestsApi.fetchTrending)
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(
+        "https://api.themoviedb.org/3/trending/all/week?api_key=2de1443070a5851805be617de2a6a73c"
+      );
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
+//   const imgMovie = axios({
+//     method: "get",
+//     url: "https://api.themoviedb.org/3/movie/95403/images?api_key=2de1443070a5851805be617de2a6a73c&language=en-US",
+//   })
+//   .then((res) => console.log(res.data.backdrops))
+//   console.log(imgMovie)
+  const imgBanner = {
+    backgroundImage: `url("https://image.tmdb.org/t/p/w500/${movie.backdrop_path}")`,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+  };
+
   return (
-    <header className="banner">
+    <header className="banner" style={imgBanner}>
       <div className="banner-container">
         <div className="banner-type-container">
-        <img src="./images/miniLogo.png" alt="" className="banner-miniLogo" />
-        <p className="banner-type">Film</p>
+          <img src="./images/miniLogo.png" alt="" className="banner-miniLogo" />
+          <p className="banner-type">{movie.media_type}</p>
         </div>
-        <h1 className="banner-title">Titre img</h1>
-        <p className="banner-description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-          facilis vitae blanditiis harum magni impedit laudantium natus
-          perferendis? Ratione, nam officiis. Necessitatibus repellat quaerat
-          animi fugit! Repellat labore deserunt aut?
-        </p>
+        <h1 className="banner-title">{movie?.title || movie?.name ||movie?.original_name}</h1>
+        <p className="banner-description">{movie.overview}</p>
         <div className="banner-btns">
           <button className="banner-btn1">
             <i className="fa-sharp fa-solid fa-play banner-icon1"></i> Lecture
--- src/config/RequestsApi.js
+++ src/config/RequestsApi.js
@@ -2,7 +2,7 @@ const API_KEY = "2de1443070a5851805be617de2a6a73c";
 const baseUrl = "https://api.themoviedb.org/3";
 
 const RequestsApi =  {
-   fetchTrending: `${baseUrl}/trending/all/week?api_keys=${API_KEY}`,
+   fetchTrending: `${baseUrl}/trending/all/day?api_keys=${API_KEY}`,
    fetchNetflixOriginals: `${baseUrl}/trending/all/week?api_keys=${API_KEY}`,
    fetchTopRated: `${baseUrl}/movie/top_rated?api_keys=${API_KEY}&language=en=US`,
    fetchActionMovies: `${baseUrl}/discover/movie?api_keys=${API_KEY}&width_genres=28`,
